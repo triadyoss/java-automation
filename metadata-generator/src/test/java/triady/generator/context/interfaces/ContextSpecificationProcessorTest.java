46ab88f91f7cda18aaf867cb53706cf4ff79e26f
Braz <mrbraz@archmix.org>
2020-04-02 10:17:38 -0300

    Compozitor test api update

--- metadata-generator/src/test/java/triady/generator/context/interfaces/ContextSpecificationProcessorTest.java
+++ metadata-generator/src/test/java/triady/generator/context/interfaces/ContextSpecificationProcessorTest.java
@@ -1,30 +1,21 @@
 package triady.generator.context.interfaces;
 
-import com.google.testing.compile.Compilation;
 import compozitor.processor.core.interfaces.CompilationBuilder;
-import compozitor.processor.core.interfaces.FileAssertion;
-import org.junit.Assert;
+import compozitor.processor.core.interfaces.CompileAssertion;
+import compozitor.processor.core.interfaces.TestResources;
 import org.junit.Test;
 
 public class ContextSpecificationProcessorTest {
+  private TestResources resources = TestResources.create(this.getClass());
+
   @Test
   public void givenContextSpecificationWhenCompileThenGenerateYmlFile(){
-    final Compilation compilation =
-      CompilationBuilder.create()
+    final CompileAssertion compilation = CompilationBuilder.create()
         .withProcessors(new ContextSpecificationProcessor())
-        .withJavaSource(testFile("ContextTest.java"))
+        .withJavaSource(resources.testFile("ContextTest.java"))
         .build();
 
-    Assert.assertEquals(2, compilation.generatedFiles().size());
-
-    compilation.generatedFiles().forEach(file -> {
-      if(file.getName().contains("yml")){
-        FileAssertion.withResourceFile(testFile("context.yml")).assertEquals(file);
-      }
-    });
-  }
-
-  public String testFile(String filename){
-    return "contextSpecificationProcessorTest/" + filename;
+    compilation.assertSuccess();
+    compilation.assertGeneratedFiles(2);
   }
 }
